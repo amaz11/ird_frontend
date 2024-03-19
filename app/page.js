8af2742b696102dcd592ee32f1849ce8f336abf6
amaz11 <k.m.shaonred@gmail.com>
2024-03-19 14:59:51 +0600

    some change

--- app/page.js
+++ app/page.js
@@ -26,7 +26,7 @@ export default function Home({ searchParams }) {
           {/* duas  */}
 
           <Suspense fallback={<Loading />}>
-            <Dua catId={searchParams.cat_id || 1} />
+            <Dua catId={searchParams.cat_id} />
           </Suspense>
 
           {/* setting  */}
