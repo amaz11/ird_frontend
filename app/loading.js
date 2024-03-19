148204b6f876a3ef27c8386fd73d438a80bd578b
amaz11 <k.m.shaonred@gmail.com>
2024-03-19 17:13:14 +0600

    loading

--- app/loading.js
+++ app/loading.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Loading = () => {
     return (
-        <div>Loading....</div>
+        <div>
+            <div className="w-12 h-12 rounded-full animate-spin
+                    border-4 border-solid border-green-500 border-t-transparent"></div>
+        </div>
     )
 }
 
